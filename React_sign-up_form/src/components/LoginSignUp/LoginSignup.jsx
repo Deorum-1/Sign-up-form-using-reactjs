b6c7f197f8f505de0348486d3152a38c238d4596
Deorum <feranmiisaac622@gmail.com>
2025-06-23 17:51:30 +0100

    Implement Login/Signup functionality with responsive styling and input placeholders

--- React_sign-up_form/src/components/LoginSignUp/LoginSignup.jsx
+++ React_sign-up_form/src/components/LoginSignUp/LoginSignup.jsx
@@ -6,29 +6,57 @@ import password_icon from "../assets/password.png";
 import email_icon from "../assets/email.png";
 
 const loginSignup = () => {
+  const [action, setAction] = React.useState("Login");
+
   return (
     <div className="container">
       <div className="header">
-        <div className="text">Sign Up</div>
+        <div className="text">{action}</div>
+        <div className="underline"></div>
       </div>
       <div className="inputs">
-        <div className="input">
-          <img src={user_icon} alt="" />
-          <input type="text" />
-        </div>
+        {action === "Login" ? (
+          <div></div>
+        ) : (
+          <div className="input">
+            <img src={user_icon} alt="" />
+            <input type="text" placeholder="Name" />
+          </div>
+        )}
         <div className="input">
           <img src={email_icon} alt="" />
-          <input type="email" />
+          <input type="email" placeholder="Email Id" />
         </div>
         <div className="input">
           <img src={password_icon} alt="" />
-          <input type="password" />
+          <input type="password" placeholder="Password" />
         </div>
       </div>
-      <div className="forgot-password">Lost Pasword? <span>Click here</span></div>
+      {action === "Sign Up" ? (
+        <div></div>
+      ) : (
+        <div className="forgot-password">
+          Lost Pasword? <span>Click here</span>
+        </div>
+      )}
+
       <div className="submit-container">
-        <div className="submit">Sign-up</div>
-        <div className="submit">Login</div>
+        <div
+          className={action === "Login" ? "submit gray" : "submit"}
+          onClick={() => {
+            setAction("Sign Up");
+          }}
+        >
+          Sign-up
+        </div>
+        <div
+          className={action === "Sign Up" ? "submit gray" : "submit"}
+          onClick={() => {
+            setAction("Login");
+          }}
+        >
+          Login
+        </div>
       </div>
     </div>
   );
